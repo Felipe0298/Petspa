a6b0ce2f4776a63714b522ad4284e0402e0da1c1
Felipe <felipe02-98@hotmail.com>
2024-07-21 14:26:23 -0500

    video 6

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -1,9 +1,21 @@
 class Authentication::UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'User was succesfully created'
+    else
+      redirect_to :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :phone, :password, :password_confirmation, :role)
   end
 end
