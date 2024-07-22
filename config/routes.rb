4ed05bbcb484120d9f1a7a6412dedcaf4335e969
Felipe <felipe02-98@hotmail.com>
2024-07-22 09:46:33 -0500

    video 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :authentication, as: '', path: '' do
     resources :users, only: [:new, :create], path: '/register', path_names: { new: '/'}
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/'}
+    
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
