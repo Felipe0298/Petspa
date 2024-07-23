0b0921588449a5a9478d8da3e55248041c29d1cd
Felipe <felipe02-98@hotmail.com>
2024-07-23 13:41:11 -0500

    video 21

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  resources :appoinments
+  namespace :authentication, as: '', path: '' do
+    resources :users, only: [:new, :create, :edit, :update], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+    resources :passwords, only: [:edit, :update], path: '/password', path_names: { edit: '/' }
+    get '/password_resets/edit', to: 'password_resets#edit', as: 'edit_password_reset'
+    resources :password_resets, only: [:new, :create], path: '/password_resets', path_names: { new: '/' }
+    patch '/password_resets', to: 'password_resets#update'
+  end
+
   resources :pets
   resources :locals
   resources :cities
-  resources :appoinment_types
-  namespace :authentication, as: '', path: '' do
-    resources :users, only: [:new, :create, :edit, :update], path: '/register', path_names: { new: '/'}
-    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/'}
-    resources :passwords, only: [:edit, :update], path: '/password', path_names: { edit: '/' }
-    get 'password_resets/edit', to:'password_resets#edit', as: 'edit_password_reset'
-    resources :password_resets, only: [:new, :create], path: '/password_resets', path_names: { new: '/' }
-    patch 'password_resets', to:'password_resets#update'
-    
-  end
+  resources :appointment_types
+  resources :appointments
+
+  get '/dashboard', to: 'home#dashboard', as: 'dashboard'
+  get '/dashboard_admin', to: 'home#dashboard_admin', as: 'dashboard_admin'
+  get '/welcome', to: 'home#welcome', as: 'welcome'
+  root to: 'home#rooting'
 
   get "up" => "rails/health#show", as: :rails_health_check
-  root 'home#welcome'
 end
